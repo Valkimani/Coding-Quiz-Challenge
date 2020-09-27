30102dae020fd7daf41f23bffd4f4af38f5f829e
Valkimani <66477953+Valkimani@users.noreply.github.com>
2020-09-27 16:23:36 -0400

    Removed the buttons created in data container and created an array for the data type choices.

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 // console.log("Hello world");
-// Taregted the id in the container and the button
+// Taregted the id in the quiz container and the button
 var quizContainer = document.getElementById("quiz");
 var startButton = document.getElementById("start-quiz");
-
-// addEventListener to listen to the button click
+var commonData = document.getElementById("common-data");
+// addEventListener to listen to the start button
 startButton.addEventListener("click", function () {
-  //style the quizConatiner id to hide it
+  //styled the quizConatiner id to hide it when the start button was clicked.
   quizContainer.style.display = "none";
 });
+
+function renderCommonDta() {
+  var dataTypes = ["1.strings", "2.booleans", " 3.alerts", "4.numbers"];
+}
+
+for (var i = 0; i < dataTypes.length; i++) {
+  1; // Create an element
+  var button = document.createElement("button");
+  2; // Add content
+  button.textContent = dataTypes[i];
+  button.setAttribute("data-value", dataTypes[i]);
+  3; //   Append to an existing element
+  commonData.append(button);
+}
