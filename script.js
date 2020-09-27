afb6cd0535b8571a116b5966ce81ccb4ba84e39e
Valkimani <66477953+Valkimani@users.noreply.github.com>
2020-09-27 14:05:38 -0400

    Added an event listener to the start button. Styled the quizContainer id to hide it when the start button was clicked.

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 // Taregted the id in the container and the button
 var quizContainer = document.getElementById("quiz");
 var startButton = document.getElementById("start-quiz");
+
+// addEventListener to listen to the button click
+startButton.addEventListener("click", function () {
+  //style the quizConatiner id to hide it
+  quizContainer.style.display = "none";
+});
