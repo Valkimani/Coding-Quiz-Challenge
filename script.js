dfd504fd6d2dbd170f5c0da3ba6946b56e6b0d1c
Valkimani <66477953+Valkimani@users.noreply.github.com>
2020-09-27 13:23:16 -0400

    Added an id to the button and targeted the buutton using getElementtById.

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 // console.log("Hello world");
+// Taregted the id in the container and the button
 var quizContainer = document.getElementById("quiz");
+var startButton = document.getElementById("start-quiz");
